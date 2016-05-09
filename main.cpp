a0d25d0bb51662487a1125b53da25d1efe07f15f
Pete Gadomski <pete.gadomski@gmail.com>
2016-05-09 08:36:37 -0600

    Beginnings of the tool

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@
 static const char USAGE[] =
     R"(Change detection for large point clouds.
 
+    I watch the ripples change their size
+    But never leave the stream
+    Of warm impermanence
+
+        - David Bowie, "Changes"
+
 Usage:
     hunky-dory cpd <source> <target>
     hunky-dory (-h | --help)
@@ -19,7 +25,7 @@ Options:
 int main(int argc, char** argv) {
     auto args = docopt::docopt(USAGE, {argv + 1, argv + argc}, true,
                                hunky_dory::VERSION);
-    std::cout << "I watch the ripples change their size\nBut never leave the "
-                 "stream\nOf warm impermanence\n";
+    std::string source_path = args["source"].asString();
+    std::string target_path = args["target"].asString();
     return 0;
 }
