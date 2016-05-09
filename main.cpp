5d701dd6b888effcd60138532252c9e924e9261d
Pete Gadomski <pete.gadomski@gmail.com>
2016-05-09 10:30:17 -0600

    Refactor

--- main.cpp
+++ main.cpp
@@ -27,8 +27,8 @@ Options:
     --no-entwine    Don't use entwine's index — use PDAL's chipper instead.
 )";
 
-std::string infer_reader_driver(const pdal::StageFactory& factory,
-                                const std::string& path);
+pdal::Stage* infer_and_create_reader(pdal::StageFactory& factory,
+                                     const std::string& path);
 
 int main(int argc, char** argv) {
     std::map<std::string, docopt::value> args = docopt::docopt(
@@ -43,17 +43,10 @@ int main(int argc, char** argv) {
 
         pdal::StageFactory factory(false);
 
-        pdal::Options source_reader_options;
-        source_reader_options.add("filename", source_path);
-        std::string source_driver = infer_reader_driver(factory, source_path);
-        pdal::Stage* source_reader = factory.createStage(source_driver);
-        source_reader->setOptions(source_reader_options);
-
-        pdal::Options target_reader_options;
-        target_reader_options.add("filename", target_path);
-        std::string target_driver = infer_reader_driver(factory, target_path);
-        pdal::Stage* target_reader = factory.createStage(target_driver);
-        target_reader->setOptions(target_reader_options);
+        pdal::Stage* source_reader =
+            infer_and_create_reader(factory, source_path);
+        pdal::Stage* target_reader =
+            infer_and_create_reader(factory, target_path);
 
         pdal::Options chipper_options;
         chipper_options.add("capacity", capacity, "");
@@ -66,7 +59,7 @@ int main(int argc, char** argv) {
         chipper.prepare(table);
 
         pdal::PointViewSet viewset = chipper.execute(table);
-        std::cout << "Point view set size: " << viewset.size() << "\n";
+
     } else {
         std::cout << "Using entwine indices\n";
         std::cout << "ERROR: not supported yet...\n";
@@ -84,4 +77,14 @@ std::string infer_reader_driver(const pdal::StageFactory& factory,
         throw std::runtime_error(ss.str());
     }
     return driver;
+}
+
+pdal::Stage* infer_and_create_reader(pdal::StageFactory& factory,
+                                     const std::string& path) {
+    pdal::Options options;
+    options.add("filename", path);
+    std::string driver = infer_reader_driver(factory, path);
+    pdal::Stage* reader = factory.createStage(driver);
+    reader->setOptions(options);
+    return reader;
 }
