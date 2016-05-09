942ee664a4853aad59c3e8d797cd963ce7d2f486
Pete Gadomski <pete.gadomski@gmail.com>
2016-05-09 08:23:12 -0600

    ClangFormat

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "hunky_dory.hpp"
 
 static const char USAGE[] =
-R"(Change detection for large point clouds.
+    R"(Change detection for large point clouds.
 
 Usage:
     hunky-dory cpd <source> <target>
@@ -16,9 +16,10 @@ Options:
     --version       Show version.
 )";
 
-int main(int argc, char** argv)
-{
-    auto args = docopt::docopt(USAGE, { argv + 1, argv + argc }, true, hunky_dory::VERSION);
-    std::cout << "I watch the ripples change their size\nBut never leave the stream\nOf warm impermanence\n";
+int main(int argc, char** argv) {
+    auto args = docopt::docopt(USAGE, {argv + 1, argv + argc}, true,
+                               hunky_dory::VERSION);
+    std::cout << "I watch the ripples change their size\nBut never leave the "
+                 "stream\nOf warm impermanence\n";
     return 0;
 }
