b147516139603231a4e9cf09ef71e218cb8d5ab2
Pete Gadomski <pete.gadomski@gmail.com>
2016-05-09 09:56:08 -0600

    Clean up option references

--- main.cpp
+++ main.cpp
@@ -13,19 +13,28 @@ static const char USAGE[] =
         - David Bowie, "Changes"
 
 Usage:
-    hunky-dory cpd <source> <target>
+    hunky-dory cpd <source> <target> [--no-entwine]
     hunky-dory (-h | --help)
     hunky-dory --version
 
 Options:
     -h --help       Show this screen.
     --version       Show version.
+    --no-entwine    Don't use entwine's index — use PDAL's chipper instead.
 )";
 
 int main(int argc, char** argv) {
-    auto args = docopt::docopt(USAGE, {argv + 1, argv + argc}, true,
-                               hunky_dory::VERSION);
-    std::string source_path = args["source"].asString();
-    std::string target_path = args["target"].asString();
+    std::map<std::string, docopt::value> args = docopt::docopt(
+        USAGE, {argv + 1, argv + argc}, true, hunky_dory::VERSION);
+    std::string source_path = args.at("<source>").asString();
+    std::string target_path = args.at("<target>").asString();
+
+    if (args["--no-entwine"]) {
+        std::cout << "Using PDAL's chipper\n";
+    } else {
+        std::cout << "Using entwine indices\n";
+        std::cout << "ERROR: not supported yet...\n";
+        return 1;
+    }
     return 0;
 }
