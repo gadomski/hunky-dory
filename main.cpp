497ff99b59844542de6054cc67d4ef7f8cfa71f2
Pete Gadomski <pete.gadomski@gmail.com>
2016-05-09 08:07:56 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "I watch the ripples change their size\nBut never leave the stream\nOf warm impermanence\n";
+    return 0;
+}
