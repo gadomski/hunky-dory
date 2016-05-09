6735d187121d0b1010857132af7304871552d19d
Pete Gadomski <pete.gadomski@gmail.com>
2016-05-09 08:20:38 -0600

    Add basic docopt structure

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+#include "docopt.h"
+#include "hunky_dory.hpp"
+
+static const char USAGE[] =
+R"(Change detection for large point clouds.
+
+Usage:
+    hunky-dory cpd <source> <target>
+    hunky-dory (-h | --help)
+    hunky-dory --version
+
+Options:
+    -h --help       Show this screen.
+    --version       Show version.
+)";
+
 int main(int argc, char** argv)
 {
+    auto args = docopt::docopt(USAGE, { argv + 1, argv + argc }, true, hunky_dory::VERSION);
     std::cout << "I watch the ripples change their size\nBut never leave the stream\nOf warm impermanence\n";
     return 0;
 }
