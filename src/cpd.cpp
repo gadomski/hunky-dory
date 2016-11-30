d3c6bab47c8fa2f52eb70c14434d07024bde295b
Pete Gadomski <pete.gadomski@gmail.com>
2016-11-30 14:56:56 -0700

    Update to match cpd

--- src/cpd.cpp
+++ src/cpd.cpp
@@ -1,4 +1,4 @@
-#include <cpd/comparer.hpp>
+#include <cpd/comparer/base.hpp>
 #include <cpd/rigid.hpp>
 #include <cpd/runner.hpp>
 
@@ -10,8 +10,8 @@ Result cpd(const Matrix& source, const Matrix& target, const DocoptMap& args) {
     double sigma2 = std::stod(args.at("--sigma2").asString());
     double outliers = std::stod(args.at("--outliers").asString());
 
-    cpd::Runner<cpd::Rigid, cpd::FgtComparer> runner;
-    runner.sigma2(sigma2).outliers(outliers);
+    cpd::Runner<cpd::Rigid> runner;
+    runner.sigma2(sigma2).outliers(outliers).comparer(cpd::Comparer::from_name("fgt"));
     cpd::Rigid::Result result = runner.run(source, target);
 
     std::cerr << "Runtime: " << result.runtime.count()
